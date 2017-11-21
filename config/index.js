e3850a650d10605adbe9c3effdf1c09c5927bb3b
Zheng Zhibo <vpma567@globalymc.com>
2017-11-21 10:01:39 +0800

    change port

--- config/index.js
+++ config/index.js
@@ -22,7 +22,7 @@ module.exports = {
   },
   dev: {
     env: require('./dev.env'),
-    port: 8080,
+    port: 8010,
     autoOpenBrowser: true,
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
